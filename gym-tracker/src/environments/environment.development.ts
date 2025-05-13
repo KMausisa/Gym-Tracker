7244d2e2adb1d5f8803723b634d8997939aea9a3
Kendrick Mausisa <kndrckm18@gmail.com>
2025-05-13 15:45:35 -0600

    Removing environment files from Git tracking

--- gym-tracker/src/environments/environment.development.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-    production: false,
-    supabaseUrl: 'https://xzorqpfrmdrycfduorzw.supabase.co',
-    supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inh6b3JxcGZybWRyeWNmZHVvcnp3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDYyMjMwNDMsImV4cCI6MjA2MTc5OTA0M30.g6cdEc1lQixexBD3JF77aL4YpGVNuUBmg8OPx_Op-II'
-};
